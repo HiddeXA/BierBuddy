7bc84b178a4acda8317280a23f37b90ba022fc90
RickvdAkkerWH <s1193906@student.windesheim.nl>
2024-11-19 12:33:06 +0100

    size change and buttons added

--- BierBuddy.UI/MainWindow.xaml.cs
+++ BierBuddy.UI/MainWindow.xaml.cs
@@ -16,17 +16,53 @@ namespace BierBuddy.UI
     /// </summary>
     public partial class MainWindow : Window
     {
-        public int NavBarSize { get; set; } = 400;
-        private readonly double PreferedSizeNavBar = 20 / 100;
+        private readonly double _SizeModifierNavBar = 0.25;
+        private readonly int _NavBarMinSize = 290;
+
+        private readonly int _MinFontSize = 26;
+        private readonly int _DefaultFontSize = 32;
+        private readonly int _FontSizeIncrement = 30;
+        private int FontSizeModifier {  get;}
+
         public MainWindow()
         {
             InitializeComponent();
+            FontSizeModifier = _MinFontSize - _NavBarMinSize / _FontSizeIncrement;
         }
-
         private void BierBuddyMainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-            NavBarSize = (int)(e.NewSize.Width * PreferedSizeNavBar);
+            if (BBMainWindow.ActualWidth * _SizeModifierNavBar > _NavBarMinSize)
+            {
+                NavBar.Width = BBMainWindow.ActualWidth * _SizeModifierNavBar;
+
+
+                int fontSize = CalculateNavBarFontSize();
+                FindBuddiesLabel.FontSize = fontSize;
+                MyBuddiesLabel.FontSize = fontSize;
+            }
+            else
+            {
+                NavBar.Width = _NavBarMinSize;
+                FindBuddiesLabel.FontSize = _MinFontSize;
+                MyBuddiesLabel.FontSize = _MinFontSize;
+            }
+            
+        }
+
+        private void FindBuddyButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MyBuddiesButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private int CalculateNavBarFontSize()
+        {
+            int fontSize = (int)(NavBar.Width / _FontSizeIncrement + FontSizeModifier);
+            return fontSize;
         }
     }
-
 }

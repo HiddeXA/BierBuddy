eb77d8e5bbffbc91cb8a0eb60422e84d93302d68
MilanDenk <150803058+MilanDenk@users.noreply.github.com>
2024-11-20 14:26:35 +0100

    Added interests
    - Interesses
    - Activiteiten
    - Drank

---
+++ BierBuddy/migrations/V002_20241120133827__Added Interests.sql
@@ -0,0 +1,197 @@
+INSERT INTO possibledrinks(Drink) VALUES('Heineken');
+INSERT INTO possibledrinks(Drink) VALUES('Hertog Jan');
+INSERT INTO possibledrinks(Drink) VALUES('Grolsch');
+INSERT INTO possibledrinks(Drink) VALUES('Jupiler');
+INSERT INTO possibledrinks(Drink) VALUES('Bavaria');
+INSERT INTO possibledrinks(Drink) VALUES('Speciaalbier');
+INSERT INTO possibledrinks(Drink) VALUES('Rode Wijn');
+INSERT INTO possibledrinks(Drink) VALUES('Witte Wijn');
+INSERT INTO possibledrinks(Drink) VALUES('Rosé');
+INSERT INTO possibledrinks(Drink) VALUES('Frisdrank');
+INSERT INTO possibledrinks(Drink) VALUES('Water');
+INSERT INTO possibledrinks(Drink) VALUES('Port');
+INSERT INTO possibledrinks(Drink) VALUES('Champagne');
+INSERT INTO possibledrinks(Drink) VALUES('Jenever');
+INSERT INTO possibledrinks(Drink) VALUES('Radler');
+INSERT INTO possibledrinks(Drink) VALUES('Advocaat');
+INSERT INTO possibledrinks(Drink) VALUES('Beerenburg');
+INSERT INTO possibledrinks(Drink) VALUES('Baileys');
+INSERT INTO possibledrinks(Drink) VALUES('Cider');
+INSERT INTO possibledrinks(Drink) VALUES('Cognac');
+INSERT INTO possibledrinks(Drink) VALUES('Gin');
+INSERT INTO possibledrinks(Drink) VALUES('Dropshot');
+INSERT INTO possibledrinks(Drink) VALUES('Goldstrike');
+INSERT INTO possibledrinks(Drink) VALUES('Jägermeister');
+INSERT INTO possibledrinks(Drink) VALUES('Licor 43');
+INSERT INTO possibledrinks(Drink) VALUES('Sambuca');
+INSERT INTO possibledrinks(Drink) VALUES('Amaretto');
+INSERT INTO possibledrinks(Drink) VALUES('Perenwijn');
+INSERT INTO possibledrinks(Drink) VALUES('Rum');
+INSERT INTO possibledrinks(Drink) VALUES('Sake');
+INSERT INTO possibledrinks(Drink) VALUES('Tequila');
+INSERT INTO possibledrinks(Drink) VALUES('Koffie met liqour');
+INSERT INTO possibledrinks(Drink) VALUES('Chocolademelk met liqour');
+INSERT INTO possibledrinks(Drink) VALUES('Vermout');
+INSERT INTO possibledrinks(Drink) VALUES('Cocktails');
+INSERT INTO possibledrinks(Drink) VALUES('Vruchtenliqour');
+INSERT INTO possibledrinks(Drink) VALUES('Whiskey');
+INSERT INTO possibledrinks(Drink) VALUES('Glühwein');
+INSERT INTO possibledrinks(Drink) VALUES('Wodka');
+INSERT INTO possibledrinks(Drink) VALUES('Feestshots');
+
+
+
+INSERT INTO possibleinterests(Interest) VALUES
+('Schilderen'),
+('Tekenen'),
+('Kalligrafie'),
+('Fotografie'),
+('Beeldhouwen'),
+('Handwerken (haken, breien, borduren)'),
+('Origami'),
+('Knutselen'),
+('DIY-projecten'),
+('Scrapbooking'),
+('Maken van sieraden'),
+('Bloemschikken'),
+('Animatie maken'),
+('Modeontwerp'),
+('Mozaïek maken'),
+('Zingen'),
+('Gitaar spelen'),
+('Piano spelen'),
+('Drummen'),
+('Elektronische muziek maken'),
+('DJ’en'),
+('Dansen (ballet, hiphop, salsa, etc.)'),
+('Acteren'),
+('Theatertechniek (licht, geluid)'),
+('Gedichten voordragen'),
+('Songwriting'),
+('Bandlid zijn'),
+('Opera zingen'),
+('Stand-upcomedy'),
+('Voetbal'),
+('Basketbal'),
+('Tennis'),
+('Hardlopen'),
+('Yoga'),
+('Fitness'),
+('Wielrennen'),
+('Mountainbiken'),
+('Klimmen'),
+('Zwemmen'),
+('Martial arts (karate, judo, etc.)'),
+('Paardrijden'),
+('Skiën/snowboarden'),
+('Surfen'),
+('Schaken'),
+('E-sports'),
+('Astronomie'),
+('Programmeren'),
+('Elektronica bouwen'),
+('Robotica'),
+('3D-printen'),
+('Wiskundige puzzels oplossen'),
+('Experimenten uitvoeren'),
+('Meteorologie'),
+('Biologie bestuderen'),
+('Cryptografie'),
+('Virtual Reality ontwikkelen'),
+('Kunstmatige intelligentie'),
+('Wandelen'),
+('Kamperen'),
+('Vogels spotten'),
+('Tuinieren'),
+('Sterren kijken'),
+('Fossielen zoeken'),
+('Vissen'),
+('Jagen'),
+('Duiken/snorkelen'),
+('Geocaching'),
+('Overlevingstechnieken (bushcraft)'),
+('Wildplukken'),
+('Lezen (fictie, non-fictie, poëzie)'),
+('Schrijven (verhalen, blogs, gedichten)'),
+('Talen leren'),
+('Boekbinden'),
+('Vertalen'),
+('Debatteren'),
+('Taalkunde bestuderen'),
+('Stripverhalen verzamelen'),
+('Koken'),
+('Bakken'),
+('Wijnproeven'),
+('Bierbrouwen'),
+('Cocktails maken'),
+('Wereldkeukens ontdekken'),
+('Voedselfotografie'),
+('Wildplukken en koken'),
+('Barista-kunsten (koffie maken)'),
+('Kaas maken'),
+('Postzegels verzamelen'),
+('Munten verzamelen'),
+('Vintage kleding'),
+('Stripboeken'),
+('Antiek'),
+('Kunstwerken'),
+('Edelstenen/mineralen'),
+('Fossielen'),
+('Kaarten en bordspellen'),
+('Sneakers'),
+('Bordspellen'),
+('Kaartspellen'),
+('Escape rooms'),
+('Videogames'),
+('Dungeons & Dragons'),
+('Sudoku'),
+('Kruiswoordpuzzels'),
+('Rubik\'s kubus oplossen'),
+('Strategische spellen'),
+('Backpacken'),
+('Culturen bestuderen'),
+('Historische locaties bezoeken'),
+('Treinreizen maken'),
+('Festivals bijwonen'),
+('Fotografie van reizen'),
+('Nieuwe talen leren'),
+('Stadswandelingen'),
+('Podcast maken'),
+('YouTube-video’s maken'),
+('Bloggen'),
+('Mediteren'),
+('Vrijwilligerswerk'),
+('Modellenbouw'),
+('LARP (Live Action Role Playing)'),
+('Autorestauratie'),
+('Minimalisme'),
+('Ruimtevaart volgen');
+
+INSERT INTO possibleactivities(Activity) VALUES
+    ('Bordspellen spelen'),
+    ('Live muziek luisteren'),
+    ('Dansen'),
+    ('Karaoke'),
+    ('Kaartspellen spelen'),
+    ('Poolen'),
+    ('Biljarten'),
+    ('Darten'),
+    ('Drankspellen spelen'),
+    ('Schaken'),
+    ('Mensen kijken'),
+    ('Gezellig kletsen'),
+    ('Roken'),
+    ('Sportwedstrijden kijken'),
+    ('Pubquiz spelen'),
+    ('Wijnproeven'),
+    ('Socializen met vreemden'),
+    ('Tafelvoetbal'),
+    ('DJ-Avond'),
+    ('Gezellig borrelen');
+
+
+
+
+
+
+

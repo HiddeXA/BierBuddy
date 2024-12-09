325142052360b7cc15908cebad5555ae35189678
MilanDenk <150803058+MilanDenk@users.noreply.github.com>
2024-12-09 12:45:18 +0100

    BierBuddyBram dummy

--- BierBuddy.Core/AlgoritmePlaceHolder.cs
+++ BierBuddy.Core/AlgoritmePlaceHolder.cs
@@ -13,15 +13,20 @@ namespace BierBuddy.Core
     {
         public Visitor GetVisitor()
         {
-            Visitor rick = new Visitor(1, "Rick", "oikos for life <3", 19);
-            rick.AddToDrinkPreference("sambuca");
-            rick.AddToDrinkPreference("speciaal bier");
-            rick.AddToInterests("oikos");
-            rick.AddToActivityPreference("oikos");
-            rick.Photos.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDpFS-Zk2p0R8q579sthM7-zf2HfFaLnkq5A&s");
-            rick.Photos.Add("https://i.imgur.com/ZySsvEN.jpeg");
-            rick.Photos.Add("https://i.scdn.co/image/ab67616d00001e022abc26e73c19483eb6d50230");
-            return rick;
+            Visitor Dummy = new Visitor(1, "BierBuddyBram", "De trotse mascotte van BierBuddy", 21);
+            Dummy.AddToDrinkPreference("Jägermeister");
+            Dummy.AddToDrinkPreference("Hertog Jan");
+            Dummy.AddToDrinkPreference("Speciaal bier");
+            Dummy.AddToDrinkPreference("Wodka");
+            Dummy.AddToInterests("Bierbrouwen");
+            Dummy.AddToInterests("Jagen");
+            Dummy.AddToInterests("Skiën");
+            Dummy.AddToActivityPreference("Gezellig kletsen");
+            Dummy.AddToActivityPreference("Darten");
+            Dummy.Photos.Add("https://i.imgur.com/DLrIGSX.png");
+            Dummy.Photos.Add("https://i.imgur.com/twGSMk7.png");
+            Dummy.Photos.Add("https://i.imgur.com/27n3fNV.png");
+            return Dummy;
         }
     }
 }

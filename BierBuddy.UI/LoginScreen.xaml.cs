4d1f1845b0a325cdc24d49b7b5f3c809684bfa1d
Hidde -_- <hiddealberts@gmail.com>
2025-01-06 21:21:21 +0100

    Update LoginScreen.xaml.cs

--- BierBuddy.UI/LoginScreen.xaml.cs
+++ BierBuddy.UI/LoginScreen.xaml.cs
@@ -28,7 +28,7 @@ namespace BierBuddy.UI
         public LoginScreen()
         {
             InitializeComponent();
-            _Connection = new MySqlConnection("server=mysql-1284b19-bierbuddy.c.aivencloud.com;database=BierBuddyDB;user=avnadmin;port=26316;password=AVNS_IWkNAbagctkfuROBkTX");
+            _Connection = new MySqlConnection("server=mysql-1284b19-bierbuddy.c.aivencloud.com;database=BierBuddyDB;user=avnadmin;port=26316;password=");
             _Connection.Open();
             _DataAccess = new MySQLDatabase(_Connection);
 
